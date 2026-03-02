280ff74d4067ef8148766b0f5a776a5840de7c3e
Frida Steen Eriksen <fridser@stud.ntnu.no>
2026-03-02 12:23:01 +0100

    finished process v1, renamed FCFSQueue to FCFSSchedule

--- src/main/java/edu/ntnu/bidata/Process.java
+++ src/main/java/edu/ntnu/bidata/Process.java
@@ -4,11 +4,15 @@ public class Process {
   private int id;
   private int arrivalTime;
   private int burstTime;
+  private int workRemaining;
+  private int completionTime;
+  private boolean finished = false;
 
   public Process(int id, int arrivalTime, int burstTime) {
     this.id = id;
     this.arrivalTime = arrivalTime;
     this.burstTime = burstTime;
+    this.workRemaining = burstTime;
   }
 
   public int getId() {
@@ -21,5 +25,23 @@ public class Process {
 
   public int getBurstTime() {
     return this.burstTime;
+  }
+
+  public int getCompletionTime() {
+    return this.completionTime;
+  }
+
+  public boolean isFinished() {
+    return finished;
+  }
+
+
+  public boolean doWork(int time) {
+    workRemaining--;
+    if (workRemaining <= 0) {
+      finished = true;
+      completionTime = time;
+    }
+    return finished;
   }
 }
