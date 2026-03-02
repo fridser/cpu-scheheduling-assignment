0ebbfb0d2fee73216c8975ee8a7d1e8286d6c10a
Frida Steen Eriksen <fridser@stud.ntnu.no>
2026-03-02 11:32:59 +0100

    Created example classes

---
+++ src/main/java/edu/ntnu/bidata/CPU.java
@@ -0,0 +1,4 @@
+package edu.ntnu.bidata;
+
+public class CPU {
+}
---
+++ src/main/java/edu/ntnu/bidata/Process.java
@@ -0,0 +1,13 @@
+package edu.ntnu.bidata;
+
+public class Process {
+  private int id;
+  private int arrivalTime;
+  private int burstTime;
+
+  public Process(int id, int arrivalTime, int burstTime) {
+    this.id = id;
+    this.arrivalTime = arrivalTime;
+    this.burstTime = burstTime;
+  }
+}
