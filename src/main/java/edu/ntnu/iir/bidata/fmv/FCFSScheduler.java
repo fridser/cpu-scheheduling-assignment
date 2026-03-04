8e686108cc71a85adf562afa3b13b92de401d490
Frida Steen Eriksen <fridser@stud.ntnu.no>
2026-03-04 13:39:34 +0100

    documented the FCFSScheduler

--- src/main/java/edu/ntnu/iir/bidata/fmv/FCFSScheduler.java
+++ src/main/java/edu/ntnu/iir/bidata/fmv/FCFSScheduler.java
@@ -3,14 +3,25 @@ package edu.ntnu.iir.bidata.fmv;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Represents a scheduler using the First Come First Serve algorithm.
+ */
 public class FCFSScheduler implements Scheduler {
   private Queue<Process> queue;
   private int time = 0;
 
+  /**
+   * Creates an instance of the FCFSScheduler.
+   */
   public FCFSScheduler(){
     queue = new LinkedList<>();
   }
 
+  /**
+   * Adds a process to the FCFSScheduler.
+   *
+   * @param process The process added to the scheduler.
+   */
   @Override
   public void addProcess(Process process) {
     queue.add(process);
