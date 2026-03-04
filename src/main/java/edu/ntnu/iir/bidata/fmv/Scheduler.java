48bef1fbfc7d399531659f73001c7d06d1f1b496
Frida Steen Eriksen <fridser@stud.ntnu.no>
2026-03-04 12:40:24 +0100

    finished documenting Scheduler interface

--- src/main/java/edu/ntnu/iir/bidata/fmv/Scheduler.java
+++ src/main/java/edu/ntnu/iir/bidata/fmv/Scheduler.java
@@ -1,11 +1,28 @@
 package edu.ntnu.iir.bidata.fmv;
 
+/**
+ * Represents a scheduler that decides when work on each process should be done
+ * in the cpu.
+ */
 public interface Scheduler {
 
+  /**
+   * Adds a process to the scheduler.
+   *
+   * @param process The process added to the scheduler.
+   */
   public void addProcess(Process process);
 
+  /**
+   * Processes one unit of time on the process first in the scheduler.
+   */
   public void process();
 
+  /**
+   * Returns whether the scheduler is empty or not.
+   *
+   * @return Whether the scheduler is empty or not.
+   */
   public boolean isEmpty();
 
 
